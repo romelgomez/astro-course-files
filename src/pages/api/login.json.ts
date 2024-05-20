914df44587295abf89da02945e591a2e088d3cfa
Chris Pennington <chris@codinginpublic.dev>
2024-05-20 11:30:36 -0600

    lesson-113

--- src/pages/api/login.json.ts
+++ src/pages/api/login.json.ts
@@ -1,18 +1,56 @@
 import type { APIRoute } from "astro";
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { projectAuth } from "../../firebase/config";
+import { validateInputs } from "../../auth/validateInputs";
+import { User, db, eq } from "astro:db";
+import { Argon2id } from "oslo/password";
+import { lucia } from "../../auth";
 
-export const POST: APIRoute = async ({ request }) => {
-  const { email, password } = await request.json();
+export const POST: APIRoute = async (context) => {
+  const { username, password } = await context.request.json();
 
   try {
-    await signInWithEmailAndPassword(projectAuth, email, password);
-    return new Response(JSON.stringify({ message: "Logged in!" }), {
-      status: 200,
-    });
+    const { success, message } = validateInputs({ username, password });
+
+    if (!success) {
+      throw new Error(message);
+    }
+
+    // does the user exist?
+    const usersMatching = await db
+      .select()
+      .from(User)
+      .where(eq(User.username, username));
+
+    if (usersMatching.length < 1) {
+      throw new Error("Incorrect username or password");
+    }
+
+    // is the password valid?
+    const validPassword = await new Argon2id().verify(
+      usersMatching[0].password_hash,
+      password
+    );
+    if (!validPassword) {
+      throw new Error("Incorrect username or password");
+    }
+
+    // create session cookie
+    const session = await lucia.createSession(usersMatching[0].id, {});
+    const sessionCookie = lucia.createSessionCookie(session.id);
+    context.cookies.set(
+      sessionCookie.name,
+      sessionCookie.value,
+      sessionCookie.attributes
+    );
+
+    return context.redirect("/");
   } catch (error) {
-    return new Response(JSON.stringify({ error }), {
-      status: 500,
-    });
+    return new Response(
+      JSON.stringify({
+        message: error instanceof Error ? error.message : error,
+      }),
+      {
+        status: 500,
+      }
+    );
   }
 };
