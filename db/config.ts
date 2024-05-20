409fcba9630497ebb9370d4dab0701a15ce2725f
Chris Pennington <chris@codinginpublic.dev>
2024-05-20 10:15:20 -0600

    lesson-109

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,23 @@
-import { defineDb } from 'astro:db';
+import { NOW, column, defineDb, defineTable } from "astro:db";
+
+const Session = defineTable({
+  columns: {
+    id: column.text({ unique: true }),
+    expiresAt: column.number(),
+    userId: column.text({ references: () => User.columns.id }),
+  },
+});
+
+const User = defineTable({
+  columns: {
+    id: column.text({ unique: true, primaryKey: true }),
+    username: column.text({ unique: true }),
+    password_hash: column.text(),
+    createdAt: column.date({ default: NOW }),
+  },
+});
 
 // https://astro.build/db/config
 export default defineDb({
-  tables: {}
+  tables: { User, Session },
 });
