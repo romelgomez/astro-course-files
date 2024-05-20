fc628b7f261c75bb974c3fd6b75a3d51518a51d8
Chris Pennington <chris@codinginpublic.dev>
2024-05-20 10:03:43 -0600

    lesson-108

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,20 @@
 import { defineMiddleware } from "astro:middleware";
-import { projectAuth } from "./firebase/config";
 
 export const onRequest = defineMiddleware((context, next) => {
-  const isLoggedIn = projectAuth.currentUser;
-  const { pathname } = context.url;
+  // const { pathname } = context.url;
 
-  if (isLoggedIn && (pathname === "/create" || pathname === "/login")) {
-    return context.redirect("/");
-  }
+  // if (isLoggedIn && (pathname === "/create" || pathname === "/login")) {
+  //   return context.redirect("/");
+  // }
 
-  if (
-    !isLoggedIn &&
-    pathname !== "/create" &&
-    pathname !== "/login" &&
-    context.request.method === "GET"
-  ) {
-    return context.redirect("/login");
-  }
+  // if (
+  //   !isLoggedIn &&
+  //   pathname !== "/create" &&
+  //   pathname !== "/login" &&
+  //   context.request.method === "GET"
+  // ) {
+  //   return context.redirect("/login");
+  // }
 
   return next();
 });
