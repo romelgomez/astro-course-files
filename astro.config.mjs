36f5e512c20c3c756203b6f05aaebe30826a3a36
Chris Pennington <chris@codinginpublic.dev>
2024-05-30 09:32:57 -0600

    lesson-132

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
   prefetch: {
     prefetchAll: true,
   },
+  devToolbar: {
+    enabled: true,
+  },
+  integrations: [react()],
 });
