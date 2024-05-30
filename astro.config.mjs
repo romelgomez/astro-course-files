c89b7381798997fd1ec4a8fb0729544399db66bf
Chris Pennington <chris@codinginpublic.dev>
2024-05-30 07:20:13 -0600

    lesson-131

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.my-site.dev",
-  base: "/new/",
-  trailingSlash: "never",
-  outDir: "./new",
-  scopedStyleStrategy: "where",
+  prefetch: {
+    prefetchAll: true,
+  },
 });
