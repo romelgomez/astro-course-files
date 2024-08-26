a43c862e6f00300f4618e5e9a969e4439ed7bc3d
Chris Pennington <chris@codinginpublic.dev>
2024-08-26 11:32:56 -0600

    lesson-135-prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,4 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  output: "hybrid",
-  adapter: vercel(),
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-  ],
-});
+export default defineConfig({});
