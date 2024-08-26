b73b728eb947e61ad75f6c0e3bd02c9f9f8b1312
Chris Pennington <chris@codinginpublic.dev>
2024-08-26 17:22:54 -0600

    lesson-138

--- keystatic.config.ts
+++ keystatic.config.ts
@@ -8,7 +8,46 @@ export default config({
     testimonial: singleton({
       label: "Testimonial Section",
       path: "src/content/keystatic/testimonialSection",
-      schema: {}      
+      schema: {
+        heading: fields.object({
+          tag: fields.select({
+            label: "HTML Tag",
+            options: [
+              {
+                label: "h1",
+                value: "h1",
+              },
+              {
+                label: "h2",
+                value: "h2",
+              },
+              {
+                label: "h3",
+                value: "h3",
+              },
+              {
+                label: "h4",
+                value: "h4",
+              },
+              {
+                label: "h5",
+                value: "h5",
+              },
+              {
+                label: "h6",
+                value: "h6",
+              },
+            ],
+            defaultValue: "h1"
+          }),
+          text: fields.text({
+            label: "Heading Text",
+            validation: {
+              isRequired: true
+            }
+          })
+        })
+      }      
     })
   }
 })
